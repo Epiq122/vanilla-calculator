ea16158221596ff42067374a503d94a83e96715d
Robert Gleason <l2ob122@gmail.com>
2025-02-08 16:55:05 -0800

    Add operator functionality to calculator for basic arithmetic operations

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const clearBtn = document.getElementById("clear-btn");
 
 let firstValue = 0;
 let awaitingNextValue = false;
+let operatorValue = "";
 
 function addNumberValue(num) {
   if (awaitingNextValue) {
@@ -16,8 +17,37 @@ function addNumberValue(num) {
   }
 }
 
+const calculate = {
+  "/": (num1, num2) => num1 / num2,
+  "+": (num1, num2) => num1 + num2,
+  "-": (num1, num2) => num1 - num2,
+  "*": (num1, num2) => num1 * num2,
+  "=": (num1, num2) => num2,
+};
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
+
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", () => addNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
   }
 });
